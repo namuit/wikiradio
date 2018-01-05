c8d87f44cf6e1b0ecd06992a13e1b7090eeefcc5
Matteo Piotto <matteo.piotto@welaika.com>
2018-01-05 21:05:17 +0100

    updated to Ruby 2.4.1, added Gemfile, added new podcast urls (2016 and 2017), etc.

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source "https://rubygems.org"
+ruby "2.4.2"
+
+#gem 'net'
+#gem 'rubygems'
+gem 'xml-simple'
+gem 'zaru'
+#gem 'date'
+#gem "fileutils"
--- wikiradio.rb
+++ wikiradio.rb
@@ -1,47 +1,74 @@
 #!/usr/bin/ruby
- 
-PODCAST_URL = 'http://www.radio.rai.it/radio3/podcast/rssradio3.jsp?id=6079'
- 
+
+# ARCHIVIO WIKIRADIO 2011-2015, 2016 e 2017
+PODCAST_URLS = [
+    'http://www.radio.rai.it/radio3/podcast/rssradio3.jsp?id=6079',
+    'http://www.radio.rai.it/rss/podcast/rssradio.jsp?channel=RF3&id=17348',
+    'http://www.radio.rai.it/rss/podcast/rssradio.jsp?channel=RF3&id=19810'
+]
+
 require 'net/http'
 require 'rubygems'
 require 'xmlsimple'
 require 'date'
 require "fileutils"
- 
-xml_data = Net::HTTP.get_response(URI.parse(PODCAST_URL)).body
-data = XmlSimple.xml_in(xml_data)
+require 'zaru'
 
-data['channel'][0]['item'].each do |item|
-    
-    podcast = item['enclosure'][0]['url']
-    title = item['title'][0]
-    description = item['description'][0]
-
-    if not title.include? " - "
-        title += description.camelcase
+def http_download_uri(uri, filename)
+  http_object = Net::HTTP.new(uri.host, uri.port)
+  http_object.use_ssl = true if uri.scheme == 'https'
+  begin
+    http_object.start do |http|
+      request = Net::HTTP::Get.new uri.request_uri
+      http.read_timeout = 500
+      http.request request do |response|
+        open filename, 'w' do |io|
+          response.read_body do |chunk|
+            io.write chunk
+          end
+        end
+      end
     end
+  rescue Exception => e
+    puts "=> Exception: '#{e}'. Skipping download."
+    return
+  end
+end
 
-    file = title.gsub("/",'-')  + ".mp3"
+def download_podcast(podcast_url)
 
-    regex='.*?((?:(?:[0-2]?\\d{1})|(?:[3][01]{1}))[-:\\/.](?:[0]?[1-9]|[1][012])[-:\\/.](?:(?:[1]{1}\\d{1}\\d{1}\\d{1})|(?:[2]{1}\\d{3})))(?![\\d])' # DDMMYYYY
+    xml_data = Net::HTTP.get_response(URI.parse(podcast_url)).body
+    data = XmlSimple.xml_in(xml_data)
 
-    m = Regexp.new(regex,Regexp::IGNORECASE)
-    date_episode = Date.strptime(m.match(title)[1],"%d/%m/%Y")
-    download_folder_episode = "wikiradio" + "/" + date_episode.year.to_s + "/" + date_episode.month.to_s + "/"
-    FileUtils::mkdir_p download_folder_episode
+    data['channel'][0]['item'].each do |item|
 
-    puts "Checking for file '" + file + "'"
-    if File::exists?(download_folder_episode + '/' + file) then
-        puts "Skipping existing file '" + file + "'"
-    else
-        puts "Downloading non-existent file '" + file + "'..."
-        file = download_folder_episode + '/' + file
-        Net::HTTP.start(URI.parse(podcast).host) do |http|
-            resp = http.get(podcast)
-            open(file, "wb") do |file|
-                puts "Writing file to disk..."
-                file.write(resp.body)
-            end
+        podcast = item['enclosure'][0]['url']
+        title = item['title'][0]
+        description = item['description'][0]
+
+        if not title.include? " - "
+            title = "#{title} - #{description}"
+        end
+
+        file_name = "#{Zaru.sanitize! title.gsub("/",'-')}.mp3"
+
+        regex='.*?((?:(?:[0-2]?\\d{1})|(?:[3][01]{1}))[-:\\/.](?:[0]?[1-9]|[1][012])[-:\\/.](?:(?:[1]{1}\\d{1}\\d{1}\\d{1})|(?:[2]{1}\\d{3})))(?![\\d])' # DDMMYYYY
+
+        m = Regexp.new(regex,Regexp::IGNORECASE)
+        date_episode = Date.strptime(m.match(title)[1],"%d/%m/%Y")
+        download_folder_episode = "wikiradio/#{date_episode.year.to_s}/#{date_episode.month.to_s}/"
+        FileUtils::mkdir_p download_folder_episode
+        puts "Checking for file '#{file_name}'"
+        file_path = "#{download_folder_episode}/#{file_name}"
+        if File::exists?(file_path) then
+            puts "Skipping existing file '#{file_name}'"
+        else
+            puts "Downloading non-existent file '#{file_name}'to #{download_folder_episode}..."
+            http_download_uri URI.parse(podcast), file_path
         end
     end
+end
+
+PODCAST_URLS.each do |podcast_url|
+    download_podcast(podcast_url)
 end
